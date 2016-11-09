a16b8d7f3cab887965f4cf7530dfa664246b6967
Sean <sean.lin@rayaegis.com>
2016-11-09 16:45:02 +0800

    [Add]Adobe Java Version Parse

--- ruby_parser.rb
+++ ruby_parser.rb
@@ -1,7 +1,43 @@
+# -*- coding: UTF-8 -*-
 require 'nokogiri'
 require 'open-uri'
 require "csv"
 
+def filter_garbled(string)
+  whitelist = []
+  string.each do |line|
+    if line.each_byte.first  < 122 then
+      whitelist << line
+    end
+  end
+  return whitelist.join
+end
+def get_java(string)
+  javaversion = string.scan(/FullVersion.*\W+REG_SZ\W+([\d\.\_\A-Za-z]+)/)
+  return  javaversion.join
+end
+def get_iplist(string)
+  ip_list = string.scan(/.*:\W+([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})/)
+  return ip_list
+end
+def get_flash(string)
+  flash_version = string.scan(/CurrentVersion.*\W+REG_SZ\W+([\d\.\_\A-Za-z\,]+)/)
+  return flash_version.join
+end
+def get_adobe(string)
+  adobe_version = string.scan(/ProductName.*\W+REG_SZ\W+(Adobe Reader[\d\.\ \_\A-Za-z\-]+)/)
+  return adobe_version.join
+end
+def get_mbsa(xml,string)
+  kblist = []
+  xml.xpath(string).each do |f|
+    #puts "#{f_windows.attributes['Name']}"
+    f.xpath("./Detail/UpdateData[@IsInstalled='false'][@Severity>=4]").each do |f_update|
+      kblist << "KB#{f_update.attributes['KBID']}"
+    end
+  end
+  return kblist
+end
   CSV.open("resule.csv", "wb") do |csv|
   csv << ["HOST Name", "Windows Update","SQL Update","Office Update", "Anti_Virus Pattern","Java Version","Adobe Verson","Adobe Flash Version"]
 
@@ -13,41 +49,55 @@ require "csv"
     doc = File.open(mfile[0]) { |f1| Nokogiri::XML(f1) }
 
     #windows update
-    w_list = []
-    doc.xpath("//Check[@Name='Windows Security Updates']").each do |f_windows|
-      #puts "#{f_windows.attributes['Name']}"
-      f_windows.xpath("./Detail/UpdateData[@IsInstalled='false'][@Severity>=4]").each do |f_update|
-        w_list << "KB#{f_update.attributes['KBID']}"
-      end
-    end
-    #puts "Windows update is #{w_list}"
+    w_list = get_mbsa(doc,"//Check[@Name='Windows Security Updates']")
     #SQL update
-    sql_list = []
-    doc.xpath("//Check[@Name='SQL Server Security Updates']").each do |f_sql|
-      #puts "#{f_sql.attributes['Name']}"
+    sql_list = get_mbsa(doc,"//Check[@Name='SQL Server Security Updates']")
 
-      f_sql.xpath("./Detail/UpdateData[@IsInstalled='false'][@Severity>=4]").each do |f_update|
-        sql_list << "KB#{f_update.attributes['KBID']}"
-      end
-    end
-    #puts "Sql update is #{sql_list}"
     #Office Update
-    off_list = []
-    doc.xpath("//Check[@Name='Office Security Updates']").each do |f_office|
-    #  puts "#{f_office.attributes['Name']}"
-      f_office.xpath("./Detail/UpdateData[@IsInstalled='false'][@Severity>=4]").each do |f_update|
-        off_list << "KB#{f_update.attributes['KBID']}"
-      end
-    end
-    #puts "Office update is #{off_list}"
+    off_list = get_mbsa(doc,"//Check[@Name='Office Security Updates']")
 
     #Java FIle
     javafiles = File.join(f,"PC", "*.java.log")
     jfile = Dir.glob(javafiles)
-    puts jfile
-    doc = File.open(jfile[0])
+    #puts jfile
+
+    javadata = File.open(jfile[0])
+    #java
+    javafile  = filter_garbled(javadata.each_char)
+    javalist  = get_java(javafile)
+    #ip
+    ipaddr = get_iplist(javafile)
+
+    #anti_virus
+    anti_files = File.join(f,"PC", "*.anti_virus.log")
+    anfile = Dir.glob(anti_files)
+    antidata = File.open(anfile[0])
+    antifile  = filter_garbled(antidata.each_char)
+    #puts antifile
+
+    #adobe
+    adobe_files = File.join(f,"PC", "*.adobe.log")
+    adfile = Dir.glob(adobe_files)
+    adobedata = File.open(adfile[0])
+    adobefile  = filter_garbled(adobedata.each_char)
+    adobe_version = get_adobe(adobefile)
+
+    #flash
+    #adobe
+    flash_files = File.join(f,"PC", "*.flash.player.log")
+    afile = Dir.glob(flash_files)
+    flashdata = File.open(afile[0])
+    flashfile  = filter_garbled(flashdata.each_char)
+    flashversion = get_flash(flashfile)
+
 
     csv << [f,w_list.count,sql_list.count,off_list.count]
-    csv << ['IP',w_list,sql_list,off_list]
+    if w_list.count == 0 then w_list << 'updated to the latest' end
+    if sql_list.count == 0 then sql_list << 'updated to the latest' end
+    if off_list.count == 0 then off_list << 'updated to the latest'  end
+    if (javalist == '')then javalist << 'No Java' end
+    if (flashversion == '')then flashversion << 'No Flash' end
+      if (adobe_version == '')then adobe_version << 'No Adobe' end
+    csv << [ipaddr.join(','),w_list.join(','),sql_list.join(','),off_list.join(','),'',javalist,adobe_version,flashversion]
   end
 end
